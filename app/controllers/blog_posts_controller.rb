a35ddc652965d4b12b8d6a6873876f10f02a9947
Felipe <felipe02-98@hotmail.com>
2023-07-12 11:45:15 -0500

    Fix nil error in blogPostController - Create

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -16,13 +16,15 @@ class BlogPostsController < ApplicationController
     end
 
     def create
-
+        @blog_post = BlogPost.new(blog_post_params)
+      
         if @blog_post.save
-            redirect_to @blog_post
+          redirect_to @blog_post
         else
-            render :new, status: :unprocessable_entity
+          render :new, status: :unprocessable_entity
         end
     end
+      
 
     def edit
     end
